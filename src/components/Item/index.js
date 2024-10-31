d6ef0e550f87cf9df1e411fa3b2e9ed44c895e09
Marcelo Amarante Cintra <est-marcelo.cintra@tjsp.jus.br>
2024-10-31 12:45:11 -0300

    Título do card

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -4,7 +4,7 @@ const Item = (props) => {
     return (
         <div className="pokeball-card">
             <div className='content'>
-
+                <h2 className='title'>Snorlax</h2>
             </div>
         </div>
     )
