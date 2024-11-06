4d87de3fb2a5e48916624d5d59d2b802d2e3fc4e
Marcelo Amarante Cintra <est-marcelo.cintra@tjsp.jus.br>
2024-11-06 10:22:23 -0300

    commit

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -2,9 +2,13 @@ import './style.css'
 
 const Item = (props) => {
     return (
-        <div className="pokeball-card">
-            <div className='content'>
-                <h2 className='title'>Snorlax</h2>
+        <div className='main-card'>
+            <h2 className='title'>Snorlax</h2>
+            <div className="pokeball-card">
+                <div className='content'>
+                    <img src='https://vignette1.wikia.nocookie.net/es.pokemon/images/9/93/Snorlax_(anime_AG).png/revision/latest?cb=20120906024823'/>
+                    <button>Detalhes</button>
+                </div>
             </div>
         </div>
     )
